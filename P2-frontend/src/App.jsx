a2ee96d1eae60260569e9fbcd96afb5df32e7bbf
Javon Martin <javonmartin63@gmail.com>
2025-09-14 15:22:27 -0400

    frontend update: page router and routes setup added

--- P2-frontend/src/App.jsx
+++ P2-frontend/src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import FlDashboard from './pages/fl-dashboard.jsx'
+import FlInvoices from './pages/fl-invoices.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='body'>
-      <FlDashboard />
+      <nav>
+        <Link to="/fl/dashboard"></Link>
+        <Link to="/fl/invoices"></Link>
+        {/* <Link to="/fl/clients">Clients</Link>
+        <Link to="/fl/reports">Reports</Link> */}
+      </nav>
+      <Routes>
+        <Route path="/fl/dashboard" element={<FlDashboard />}></Route>
+        <Route path="/fl/invoices" element={<FlInvoices />}></Route>
+        {/* <Route path="/fl/clients" element={<FlClients />}></Route> */}
+        {/* <Route path="/fl/reports" element={<FlReports />}></Route> */}
+      </Routes>
     </div>
   )
 }
---
+++ P2-frontend/src/pages/fl-invoices.jsx
@@ -0,0 +1,11 @@
+import '../styles/fl-invoices.css'
+
+function FlInvoices() {
+    return (
+        <div>
+            <h1>HELLO WORLD</h1>
+        </div>
+    )
+}
+
+export default FlInvoices

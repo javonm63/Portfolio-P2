b5980a7f3cf6b7ba15c3e301d7e015f48605b4c3
Javon Martin <javonmartin63@gmail.com>
2025-09-18 00:36:03 -0400

    frontend update: client reports darkmode added

--- P2-frontend/src/pages/cl-reports.jsx
+++ P2-frontend/src/pages/cl-reports.jsx
@@ -5,6 +5,8 @@ import ClWebNavbar from "../components/clWebNav.jsx";
 import ReportsGraphCard from '../components/reportsGraphCard.jsx';
 import PieGraphCard from '../components/pieGraph.jsx';
 import TotalsCard from '../components/reportsTotalsCard.jsx';
+import { useEffect } from 'react';
+import { showDarkModeHook } from '../hooks/landingPageHooks.jsx';
 
 function ClReports() {
     const navbarHook = navbarHooks() 
@@ -13,26 +15,36 @@ function ClReports() {
     const sideNav = navbarHook.sideNav
     const setSideNav = navbarHook.setSideNav
 
+    const darkModeHook = showDarkModeHook()
+    const darkMode = darkModeHook.darkMode
+    const setDarkMode = darkModeHook.setDarkMode
+
+    useEffect(() => {
+        if (window.matchMedia('(prefers-color-scheme : dark)').matches) {
+            setDarkMode(true)
+        }
+    })
+
     return (
         <div className='flReports-page-container'>
             <Searchbar sideNav={sideNav} setSideNav={setSideNav} setShowWebNav={setShowWebNav} />
             <div className="page-title-container">
-                <h1 className="page-titles">REPORTS</h1>
+                <h1 className={darkMode ? "page-titles" : "page-titles dark"}>REPORTS</h1>
             </div>
             <ClWebNavbar showWebNav={showWebNav} />
             <div className='reports-subtitles-container'>
-                <h2 id="monthly-title" className="reports-page-sub-titleL">OVERDUE PER MONTH</h2>
-                <h2 id="PvsUP-title" className="reports-page-sub-titleR">PAID VS. UNPAID</h2>
+                <h2 id="monthly-title" className={darkMode ? "reports-page-sub-titleL dark" : "reports-page-sub-titleL"}>OVERDUE PER MONTH</h2>
+                <h2 id="PvsUP-title" className={darkMode ? "reports-page-sub-titleR dark" : "reports-page-sub-titleR"}>PAID VS. UNPAID</h2>
             </div>
             <div className='reports-main-container'>
-                <div className='reports-left-container'>
+                <div className={darkMode ? 'reports-left-container dark' : 'reports-left-container'}>
                     <ReportsGraphCard graphLabel={"Monthly Earnings"}/>
                 </div>
-                <div className='reports-right-container'>
+                <div className={darkMode ? 'reports-right-container dark' : 'reports-right-container'}>
                     <PieGraphCard />
                 </div>
             </div>
-            <TotalsCard totalTitle={"Total Spent"}/>
+            <TotalsCard darkMode={darkMode} totalTitle={"Total Spent"}/>
         </div>
     )
 }

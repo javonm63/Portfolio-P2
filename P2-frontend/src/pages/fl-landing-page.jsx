f54dfe22d21ea009c4ee70bbfb5db4a27a29161c
Javon Martin <javonmartin63@gmail.com>
2025-09-18 00:57:33 -0400

    frontend update: freelancer landing darkmode added

--- P2-frontend/src/pages/fl-landing-page.jsx
+++ P2-frontend/src/pages/fl-landing-page.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import '../styles/fl-landing-page.css'
 import {navbarHooks, searhbarHooks, showNotifHook} from "../hooks/fl-dashboardHooks.jsx";
-import { showSignUpHook, showLoginHook } from '../hooks/landingPageHooks.jsx'
+import { showSignUpHook, showLoginHook, showDarkModeHook } from '../hooks/landingPageHooks.jsx'
 import Landingbar from "../components/landingBar.jsx";
 import ClInvPgMenuCard from "../components/CLinvoicePageMenu.jsx";
 import LandingCard1 from '../components/flLandingCard1.jsx';
@@ -32,6 +32,16 @@ function FlLandingPage() {
     const loginHooks = showLoginHook()
     const showLogin = loginHooks.showLogin
     const setShowLogin = loginHooks.setShowLogin
+
+    const darkModeHook = showDarkModeHook()
+    const darkMode = darkModeHook.darkMode
+    const setDarkMode = darkModeHook.setDarkMode
+
+    useEffect(() => {
+        if (window.matchMedia('(prefers-color-scheme : dark)').matches) {
+            setDarkMode(true)
+        }
+    })
 
     const showSendInvPg = () => {
         window.location.href = '/cl'
@@ -76,12 +86,12 @@ function FlLandingPage() {
         <div className='landing-page-main-cont'>
             <Landingbar openSignup={openLoginPg} sideNav={sideNav} setSideNav={setSideNav} setShowWebNav={setShowWebNav} />
             <div className="page-title-container">
-                <h1 className="page-titles">FREELANCER</h1>
+                <h1 className={darkMode ? "page-titles" : "page-titles dark"}>FREELANCER</h1>
             </div>
             <ClInvPgMenuCard display={display} showSend={showSendInvPg} showNewPg={showNewInvPg} showAll={showAllInvPg} submenuText1={'FREELANCER'} submenuText2={'CLIENT'} submenuText3={'LOGIN'}/>
-            <LandingCard1 openSignup={openSignupPg} card1Title={'Simplify your invoicing'} card1Text={'An easy to use invoicing and billing software that connects freelancers and clients.'} card1Btn={'Sign Up'}/>
-            <LandingCard2 card2Title={'Create invoices'} card2Text={'Create professional invoices in minutes with just a few clicks.'} card2Title2={'Manage clients'} card2Text2={'Add clients profiles for e-invoice features and clients management.'} card2Title3={'Track payments'} card2Text3={'Easily track which invoices aare paid, overdue, sent/emailed plus more.'} card2Title4={'Stay organized'} card2Text4={'Manage clients, invoices, payments all in one place.'}/>
-            <LandingCard3 graphLabel={'Monthly Earnings'} openSignup={openSignupPg} landingCard3Text={'You can also view insights about your invoices and clients such as paid vs unpaid invoices, monthly earnings, outstanding balances etc.'}/>
+            <LandingCard1 darkMode={darkMode} openSignup={openSignupPg} card1Title={'Simplify your invoicing'} card1Text={'An easy to use invoicing and billing software that connects freelancers and clients.'} card1Btn={'Sign Up'}/>
+            <LandingCard2 darkMode={darkMode} card2Title={'Create invoices'} card2Text={'Create professional invoices in minutes with just a few clicks.'} card2Title2={'Manage clients'} card2Text2={'Add clients profiles for e-invoice features and clients management.'} card2Title3={'Track payments'} card2Text3={'Easily track which invoices aare paid, overdue, sent/emailed plus more.'} card2Title4={'Stay organized'} card2Text4={'Manage clients, invoices, payments all in one place.'}/>
+            <LandingCard3 darkMode={darkMode} graphLabel={'Monthly Earnings'} openSignup={openSignupPg} landingCard3Text={'You can also view insights about your invoices and clients such as paid vs unpaid invoices, monthly earnings, outstanding balances etc.'}/>
 
             <FlSignUp openSignup={showSignup} closeSignup={closeSignup}/>
             <LoginCard showSignup={showLogin} closeSignup={closeSignup} />
